cadf464644bfdb91206882e7443106e1afbc18df
Taibu Haroub <haroubtaibu@gmail.com>
2022-01-20 02:34:38 +0300

    Add small cards

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
+import SmallCard from "../components/SmallCard";
 
-export default function Home() {
+export default function Home({ exploreData }) {
   return (
     <div className="">
       <Head>
@@ -16,6 +17,48 @@ export default function Home() {
 
       {/* Banner  */}
       <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+
+          {/* pull some data from a server - API endpoint */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map((item) => (
+              <SmallCard
+                key={item.img}
+                img={item.img}
+                location={item.location}
+                distance={item.distance}
+              />
+            ))}
+          </div>
+        </section>
+
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+        </section>
+      </main>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  // alternative url: https://links.papareact.com/pyp
+  try {
+    const data = await fetch("https://jsonkeeper.com/b/4G1G");
+    const exploreData = await data.json();
+    return {
+      props: {
+        exploreData,
+      },
+    };
+  } catch (error) {
+    console.log(error.message);
+  }
+  // return {
+  //   props: {
+  //     exploreData,
+  //   },
+  // };
 }
